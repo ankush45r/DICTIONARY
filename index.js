ca4e4f545278fa0d54677b36b4d0960bf936cc5d
ankush45r <130866395+ankush45r@users.noreply.github.com>
2023-04-23 05:23:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -148,4 +150,4 @@ app.post('/signup_', async (req,res)=>{
     }
 })
 
-app.listen(3000);
+app.listen(port);
